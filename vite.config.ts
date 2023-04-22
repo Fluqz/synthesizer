f91e6341a3ef5d1b0a4f0cc5b9eff9fe90654a84
Flux <Fluqz@users.noreply.github.com>
2023-04-22 19:40:39 +0200

    upload

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://fluqz.github.io/',
+  // base: 'https://fluqz.github.io/',
+  base: '/Synthesizer/',
   plugins: [
     svelte({
       // onwarn: (warning, defaultHandler) => {
